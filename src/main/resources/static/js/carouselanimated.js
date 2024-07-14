9332e959ca84e2e7aca9d09728af974217a54cf2
millertime90 <scott.j.miller0727@gmail.com>
2024-07-14 12:56:14 -0500

    configured classname assignment for slide in animation as per click on carousel inidcators

--- src/main/resources/static/js/carouselanimated.js
+++ src/main/resources/static/js/carouselanimated.js
@@ -5,11 +5,14 @@ $(document).ready(function() {
 	let ccp = document.querySelector(".carousel-control-prev"); 
 	let ccn = document.querySelector(".carousel-control-next"); 
 	let h5s = document.querySelectorAll(".carousel-item h5"); 
-	
-	let hasClassName = (elems, className) => Array.from(elems).every(elem => elem.classList.contains(className)); 
+	let lis = document.querySelectorAll(".carousel-indicators li"); 
+	 
 	let slideInFromLeftClassName = "slideInFromLeft"; 
 	let slideInFromRightClassName = "slideInFromRight"; 
 	
+	let hasClassName = (elems, className) => Array.from(elems).every(elem => elem.classList.contains(className)); 
+	
+	// slideInFrom{Left/Right} class assigned as per carousel control button clicked 
 	$(ccp).click(function() {
 		
 		if(hasClassName(h5s, slideInFromLeftClassName)) {
@@ -29,6 +32,39 @@ $(document).ready(function() {
 				  .addClass(slideInFromLeftClassName); 
 			
 		} 
+		
+	}); 
+	
+	// slideInFrom{Left/Right} class assigned as per indicator li element clicked 
+	lis.forEach(li => {
+		
+		li.addEventListener("click", e => {
+			
+			let activeLi = Array.from(lis).filter(i => i.classList.contains("active"))[0]; 
+			let toBeActiveLi = Array.from(lis).filter(i => i == e.target)[0]; 
+			
+			if(Number(toBeActiveLi.dataset.slideTo) > Number(activeLi.dataset.slideTo)) {
+				
+				if(hasClassName(h5s, slideInFromRightClassName)) {
+					
+					$(h5s).removeClass(slideInFromRightClassName)
+						  .addClass(slideInFromLeftClassName); 
+					
+				}
+				
+			} 
+			else {
+				
+				if(hasClassName(h5s, slideInFromLeftClassName)) {
+					
+					$(h5s).removeClass(slideInFromLeftClassName)
+						  .addClass(slideInFromRightClassName); 
+					
+				} 
+				
+			}
+			
+		}); 
 		
 	}); 
 
