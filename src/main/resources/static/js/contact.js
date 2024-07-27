22799c6313815e0a651fdefcdbae40bba89879e4
millertime90 <scott.j.miller0727@gmail.com>
2024-07-27 15:22:12 -0500

    Still to be completed
    jQuery/CSS slide-in/out animations on contact elements in contact section.

---
+++ src/main/resources/static/js/contact.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	
+	let mediaOT = $(".contact-col .pt-1").offset().top,
+		mediaOH = $(".contact-col .pt-1").outerHeight(),
+		contactOT = $(".contact-col .pt-3").offset().top,
+		contactOH = $(".contact-col .pt-3").outerHeight(),
+		wh = $(window).height(),
+		wst = $(window).scrollTop(); 
+		
+	if(wst > (mediaOT + mediaOH - wh)) {
+		
+		
+		
+	}
+	
+}); 
