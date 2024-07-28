50406b57ff38756441e04d40ee91fd27f6d8bdef
millertime90 <scott.j.miller0727@gmail.com>
2024-07-28 14:42:12 -0500

    slide in effects added to contact section elements

--- src/main/resources/static/js/contact.js
+++ src/main/resources/static/js/contact.js
@@ -1,16 +1,36 @@
 $(document).ready(function() {
 	
-	let mediaOT = $(".contact-col .pt-1").offset().top,
-		mediaOH = $(".contact-col .pt-1").outerHeight(),
-		contactOT = $(".contact-col .pt-3").offset().top,
-		contactOH = $(".contact-col .pt-3").outerHeight(),
-		wh = $(window).height(),
-		wst = $(window).scrollTop(); 
+	$(window).scroll(function() {
+	
+		let mediaOT = $(".contact-col .pt-1").offset().top,
+			mediaOH = $(".contact-col .pt-1").outerHeight(),
+			contactOT = $(".contact-col .pt-3").offset().top,
+			contactOH = $(".contact-col .pt-3").outerHeight(),
+			wh = $(window).height(),
+			wst = $(window).scrollTop(); 
 		
-	if(wst > (mediaOT + mediaOH - wh)) {
+		if(wst > (mediaOT + mediaOH - wh)) {
 		
+			$(".contact-col .pt-1").removeClass("slideOutLeft").addClass("slideInLeft"); 
 		
+		} 
+		else {
+
+			$(".contact-col .pt-1").removeClass("slideInLeft").addClass("slideOutLeft"); 			
+			
+		} 
 		
-	}
+		if(wst > (contactOT - wh)) {
+			
+			$(".contact-col .pt-3").removeClass("slideOutRight").addClass("slideInRight"); 
+			
+		} 
+		else {
+			
+			$(".contact-col .pt-3").removeClass("slideInRight").addClass("slideOutRight"); 
+			
+		}
+	
+	}); 
 	
 }); 
