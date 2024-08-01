4aba19943cbd056790a706c3ed49b8d10244f801
millertime90 <scott.j.miller0727@gmail.com>
2024-07-31 21:19:24 -0500

    Created sign up form modal

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,12 +3,20 @@
 $(document).ready(function() {
 
 	let adModal = document.querySelector("#adModal"); 
+	let signupModal = document.querySelector("#signupModal"); 
+	
 	let adPopup = setInterval(function() {
 		
 		$(adModal).modal("show"); 
 		clearInterval(adPopup); 
 		
-	}, 4000); 
+	}, 3000); 
+	
+	$(adModal).click(function() {
+		
+		$(signupModal).modal("show"); 
+		
+	}); 
 
 	let cr = document.querySelector("#cr"); 
 	let d = new Date(); 
